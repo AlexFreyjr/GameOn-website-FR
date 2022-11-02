95e590abda102b9c9b96cdc0c91a88d44035d2e2
AlexFreyjr <alexfreyjr@gmail.com>
2022-11-02 16:39:17 +0100

    70% verif

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,16 +12,17 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
-const errorMsg = document.querySelector(".error");
 const formName = document.getElementById("first");
 const formSurname = document.getElementById("last");
 const formMail = document.getElementById("email");
 const formBirth = document.getElementById("birthdate");
 const formTournament = document.getElementById("quantity");
-const submitBtn = document.querySelector(".btn-submit");
-const nameIsValid = false;
-const mailIsValid = false;
-const dateIsValid = false;
+const formCheck = document.querySelectorAll(".checkbox-input");;
+let nameIsValid = 0;
+let mailIsValid = false;
+let dateIsValid = false;
+let tournamentIsValid = false;
+let checkIsValid = 0;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -39,14 +40,24 @@ function closeModal() {
 }
 
 //name validation
-formName.addEventListener('change', nameVerification);
-formSurname.addEventListener('change', nameVerification);
-function nameVerification (e){
+formName.addEventListener('change', fnameVerification);
+function fnameVerification (e){
   if(e.target.value.length < 2 ) { 
-    errorMsg.inner = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    document.querySelector(".error-first").innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
   }
   else{
-    nameIsValid = true;
+    document.querySelector(".error-first").style.visibility = "hidden";
+    nameIsValid += 1;
+  }
+}
+formSurname.addEventListener('change', lnameVerification);
+function lnameVerification (e){
+  if(e.target.value.length < 2 ) { 
+    document.querySelector(".error-last").innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+  }
+  else{
+    document.querySelector(".error-last").style.visibility = "hidden";
+    nameIsValid += 1;
   }
 }
 //mail validation
@@ -55,39 +66,60 @@ function mailVerification (e){
   let mailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
   if (mailRegExp.test(e.target.value) == false)
   {
-    alert("L'adresse mail n'est pas valide.")
+    document.querySelector(".error-mail").innerHTML = "Veuillez entrer une adresse mail valide.";
   }
   else {
+    document.querySelector(".error-mail").style.visibility = "hidden";
     mailIsValid = true;
   }
 }
 formBirth.addEventListener('change', birthVerification);
 function birthVerification (e){
-//check is a valid date
+  let now = new Date();
+  let date = e.target.value;
+  //let age = now.getFullYear() - date.getFullYear();
+  if (date == null || date > now){
+    document.querySelector(".error-birth").innerHTML = "Vous devez entrer votre date de naissance.";
+  }
+  else {
+    document.querySelector(".error-birth").style.visibility = "hidden";
+    dateIsValid = true;
+  }
 }
 
 //participation validation
-formTournament.addEventListener('change', tournamentValidation)
+formTournament.addEventListener('change', tournamentValidation);
 function tournamentValidation (e){
-  if(e.target.value === null || e.target.value === "" ) { 
-    alert("Veuillez entrer un nombre");
+  console.log(e);
+  if(e.target.value == null || e.target.value == " " ) { 
+    document.querySelector(".error-tournament").innerHTML = "Vous devez choisir une option.";
+  }
+  else {
+    tournamentIsValid = true;
+    document.querySelector(".error-tournament").style.dvisibility = "hidden";
   }
 }
 
 //check radio 
-
+//for i in formCheck {
+//  if(i.isChecked) {
+//    if(i == 0) { checkIsValid += 1;}
+//    else { document.querySelector(".error-checkbox").innerHTML = "Vous devez choisir une seule option.";}
+//}
+// else { document.querySelector(".error-checkbox").innerHTML = "Vous devez choisir une seule option.";}
+//}
 
 
 //form submitting
-submitBtn.addEventListener("submit", (e) => {
-  console.log(e);
+modalbg.addEventListener("submit", validate); 
+function validate(e) {
   e.preventDefault();
-  if (nameIsValid && mailIsValid && dateIsValid){
-    alert("Merci ! Votre réservation a été reçue.")
+  if (nameIsValid == 2 && mailIsValid == true && dateIsValid == true && tournamentIsValid == true){
+    alert("Merci ! Votre réservation a été reçue.");
   }
   else{
-    alert("please correct")
+    alert("Merci de remplir correctement le formulaire");
   }
-});
+};
 
 
